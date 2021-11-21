9b224a7644ab6283e77c11b3c02753ccff202c15
tair77 <tair.baratov77@gmail.com>
2021-11-21 19:06:52 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main (String[] args ){
+            Boss nike = new Boss(9999,888,"the zachita");
+        System.out.println(" Boss.health " +  nike.getHealth() + " " + nike.getDamage() + " " + nike.getBossDeafanse ());
+    }
+}
+
